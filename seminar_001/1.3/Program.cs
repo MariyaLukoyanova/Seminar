358478b03aa19c28f70a36c036f9ab11042a7679
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-11-27 18:44:19 +0300

    Task 1.3

---
+++ seminar_001/1.3/Program.cs
@@ -0,0 +1,11 @@
+﻿// На вход принимает число N, 
+// на выход показывает все целые число в промежутке от -N до N
+
+Console.WriteLine("Введите число N ");
+int N = int.Parse(Console.ReadLine());
+int count = - N;
+while(count <= N)
+{
+    Console.Write($"{count}, ");
+    count = count + 1;
+}
