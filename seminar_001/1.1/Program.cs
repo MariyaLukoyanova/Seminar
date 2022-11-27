2423bef796975b8563000ceef7442302fd9f2a1f
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-11-27 18:17:02 +0300

    Task 1.1

---
+++ seminar_001/1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// На вход принимает два числа и проверяет, 
+//является ли первое число квадратом второго.
+
+Console.WriteLine("Введине первое число ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введине второе число ");
+int b = int.Parse(Console.ReadLine());
+
+if(a == b*b)
+{
+    Console.WriteLine("Является");
+}
+else
+    Console.WriteLine("Не является");
