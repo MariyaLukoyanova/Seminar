b28118d9200bfd7a85b6e5a9cca22fc6adf3b467
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-11-27 20:12:35 +0300

    Home task 4

---
+++ Homework_01/HW_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+if(num <=0)
+{
+    Console.WriteLine("Ошибка ввода - только положительные числа!");
+}
+int count = 1;
+while (count <= num)
+{
+    if(count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+count++;
+}
+
