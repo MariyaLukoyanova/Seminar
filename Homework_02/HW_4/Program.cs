26d735fb18fa5dcebcbdf0f8c77f1eb0e21bb76d
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-12-04 17:41:12 +0300

    HW_4

---
+++ Homework_02/HW_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void Weekend(int arg)
+{
+    if(arg < 1 || arg > 7)
+    {Console.WriteLine("Такого дня не существует :/");}
+    else
+    {
+        if (arg == 6 || arg == 7)
+            Console.WriteLine("Это выходной :)");
+        else
+            Console.WriteLine("Это не выходной :(");
+    }
+}
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+Weekend(num);
