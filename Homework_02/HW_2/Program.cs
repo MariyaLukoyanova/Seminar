9d2801a9019374de4efbafd9298e337be4a07251
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-12-04 17:25:17 +0300

    HW_2

---
+++ Homework_02/HW_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+int Del2(int arg)
+{
+    Console.WriteLine(arg);
+    arg = (arg / 100) * 10 + (arg % 10);
+    return arg; 
+}
+Console.WriteLine(Del2(new Random().Next(100, 1000)));
