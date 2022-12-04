6414878974072202dcf4c6f6caa9df605141fd1f
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-12-04 17:31:16 +0300

    HW_3

---
+++ Homework_02/HW_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+
+
+void Num3(int arg)
+{if (arg < 100 || arg > 999)
+    {
+        Console.WriteLine("Третьей цифры нет!");
+    }
+    else 
+    {
+        Console.WriteLine($"Третья цифра {arg % 10}");
+    }
+}
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+Num3(num);
+
