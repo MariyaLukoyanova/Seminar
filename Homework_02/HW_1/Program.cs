c53be02f7a873b0e924cad8c2deae710158f4370
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-12-04 17:19:44 +0300

    HW_1

---
+++ Homework_02/HW_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру 
+// этого числа.
+
+
+
+int Num(int arg)
+{while(arg < 100 || arg > 999)
+    {
+        Console.WriteLine("Число не трехзначное!");
+        Console.WriteLine("Введите трехзначное число");
+        arg = int.Parse(Console.ReadLine());
+    }
+        return arg;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Вторая цифра в числе {(Num(num)%100)/10}");
