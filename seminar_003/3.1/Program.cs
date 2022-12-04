05007756bab84996b49152adf270e4e82da91214
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-12-04 19:28:01 +0300

    Task 3.1

---
+++ seminar_003/3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+void Quarters(string arg)
+{
+    if(arg == "I") 
+        Console.WriteLine("x > 0, y > 0");
+    else if(arg == "II")
+        Console.WriteLine("x < 0, y > 0");
+    else if(arg == "III")
+        Console.WriteLine("x < 0, y < 0");
+    else if(arg == "IV")
+        Console.WriteLine("x > 0, y < 0");
+    else
+    Console.WriteLine("Такой четверти не существует");
+}
+Console.WriteLine("Введите номер четверти");
+Quarters(Console.ReadLine());
