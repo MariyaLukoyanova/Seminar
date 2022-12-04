a8ec1897a873ecd4448bd4b734a493f287f1efda
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-12-04 16:42:06 +0300

    Task 2.3

---
+++ seminar_002/2.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+void Krat(int arg)
+{
+    if ((arg % 7 == 0)&(arg % 23 ==0))
+        {Console.WriteLine("Число кратно 7 и 23");}
+    else
+        {Console.WriteLine("Число не кратно 7 и 23");}
+}
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+Krat(num);
