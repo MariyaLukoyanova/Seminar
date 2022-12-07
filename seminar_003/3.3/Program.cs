1c64a2c3c92974d52dc1e50dcb4e3ee2e277c3a7
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-12-07 16:32:28 +0300

    Task 3.3

---
+++ seminar_003/3.3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+
+void Sqr(int n)
+{
+int index = 1;
+while (index <= n)
+{
+    Console.Write($"{index*index}, ");
+    index++;
+}
+}
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+Sqr(N);
