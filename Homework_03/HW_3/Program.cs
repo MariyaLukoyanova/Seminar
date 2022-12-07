2a5c044334a25f8ef8a29d218fe7e13d1a22cf7a
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-12-07 18:05:18 +0300

    hw_3

---
+++ Homework_03/HW_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Sqr(int n)
+{
+int index = 1;
+while (index <= n)
+{
+    Console.Write($"{Math.Pow(index, 3)}, ");
+    index++;
+}
+}
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+Sqr(N);
