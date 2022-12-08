d2d9b99adaa0daf2340e3ab9a805e6d4676177d1
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-12-08 17:09:37 +0300

    HW_2

---
+++ Homework_04/HW_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+int Num(int arg)
+{
+    arg = Math.Abs(arg);
+    if (arg == 0) return 0;
+    int count = 0;
+    while (arg > 0)
+    {
+        count = count + arg % 10;
+        arg = arg / 10;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число");
+Console.WriteLine(Num(int.Parse(Console.ReadLine())));
