8aca41508bba050cbe2d179e341bd88094818afb
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-12-14 21:55:12 +0300

    6.2

---
+++ seminar_006/6.2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+
+string Transform(int num)
+{
+    string result = null;
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+Console.WriteLine(Transform(int.Parse(Console.ReadLine())));
