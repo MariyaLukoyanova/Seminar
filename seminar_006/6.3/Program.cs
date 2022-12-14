73278eb0f4c42a0eec4be72945e43887a0f54705
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2022-12-14 22:55:57 +0300

    6.4

--- seminar_006/6.3/Program.cs
+++ seminar_006/6.3/Program.cs
@@ -3,12 +3,12 @@
 
 void Fib(int N)
 {
-    int n1=0, n2=1;
-    for (int i =0; i<N; i++)
-{
-    Console.Write($"{n1} ");
-    (n1, n2)=(n2, n1+n2);
-}
+    int n1 = 0, n2 = 1;
+    for (int i = 0; i < N; i++)
+    {
+        Console.Write($"{n1} ");
+        (n1, n2) = (n2, n1 + n2);
+    }
 }
 Console.WriteLine("Введите число");
 Fib(int.Parse(Console.ReadLine()));
