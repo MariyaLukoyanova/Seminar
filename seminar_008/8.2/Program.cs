9a1160c348bdbbaf8056fc97f4f2497b91916022
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2023-01-04 20:34:47 +0300

    seminar_08

---
+++ seminar_008/8.2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив. Напишите программу,которая 
+// заменяет строки на столбцы. В случае, если это невозможно, 
+// программа должна вывести сообщение для пользователя.
+
+int[,] Massiv(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        {
+            for (int j = 0; j < n; j++)
+                array[i, j] = new Random().Next(-99, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ChangeRawForColumn(int[,] array)
+{
+    if (array.GetLength(1) != array.GetLength(0)) Console.WriteLine("Это невозможно!");
+    else
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < i; j++)
+                (array[i, j], array[j, i]) = (array[j, i], array[i, j]);
+        }
+}
+
+Console.WriteLine("Введите m*n массива");
+int[,] array = Massiv(int.Parse(Console.ReadLine()),
+                    int.Parse(Console.ReadLine()));
+PrintArray(array);
+ChangeRawForColumn(array);
+PrintArray(array);
---
+++ seminar_008/8.4/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайтедвумерный массив из целых чисел.
+// Напишите программу, которая удалит строку и столбец, на
+// пересечении которых расположен наименьший элемент массива.
+
+int[,] Massiv(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        {
+            for (int j = 0; j < n; j++)
+                array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void DeleteMin(int[,] array)
+{
+    int min = array[0, 0];
+    int[] minIndex = new int[2];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < min)
+            {
+                min = array[i, j];
+                minIndex[0] = i; minIndex[1] = j;
+            }
+        }
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == minIndex[0] | j == minIndex[1]) continue;
+            Console.Write($"{array[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.WriteLine("Введите m*n массива");
+int[,] array = Massiv(int.Parse(Console.ReadLine()),
+                    int.Parse(Console.ReadLine()));
+PrintArray(array);
+DeleteMin(array);
