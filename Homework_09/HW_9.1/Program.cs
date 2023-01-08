ac53742ec0830b05537040eefad36e45c568f159
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2023-01-08 18:21:01 +0300

    homework 9

---
+++ Homework_09/HW_9.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все
+// чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+void FromMtoN(int m, int n)
+{
+    if (m > n) return;
+    if ((m > 0) && (m % 2 == 0)) Console.Write($"{m}, ");
+    FromMtoN(m + 1, n);
+}
+
+
+
+Console.WriteLine("Enter M & N");
+FromMtoN(int.Parse(Console.ReadLine()),
+int.Parse(Console.ReadLine()));
---
+++ Homework_09/HW_9.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+
+int FromMtoN(int m, int n)
+{
+    if (m > n) return 0;
+    return m + FromMtoN(m + 1, n);
+}
+
+
+
+Console.WriteLine("Enter M & N");
+Console.WriteLine(FromMtoN(int.Parse(Console.ReadLine()),
+int.Parse(Console.ReadLine())));
